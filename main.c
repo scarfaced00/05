38564cbbf1574386f167cd2ccc778c1d5c780bd8
scarfaced00 <jazmyne@naver.com>
2020-10-07 14:25:11 +0900

    ex3

--- main.c
+++ main.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* ex2 */
+/* ex3 */
 
 int main(int argc, char *argv[]) {
-	
-	int num;
-	printf("정수 하나를 입력하시오 :");
-	scanf("%d",&num);
-	
-	if(num>0)
-		printf("절대값은 %d 입니다.",num);
-	else if(num<0)
-		printf("절대값은 %d 입니다.",-num);
-	else
-		printf("절대값은 %d 입니다.",num); 
+
+	int cnt = 0;
+	char c;
+	printf("Input a string:");
+	while ((c = getchar()) != '\n')
+	{
+		if (48 <= c && c <= 57)
+			cnt++;
+	}
+
+	printf("the number of digits is %d", cnt);
+
 	return 0;
 }
